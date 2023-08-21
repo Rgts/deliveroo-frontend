9679e91debab6c989ee9fcbc61aabb0679f83cec
Renaud Gantois <renaud.gantois@gmail.com>
2023-08-21 21:12:23 +0200

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import logoDeliveroo from "./assets/images/logoDeliveroo.png"
 
 function App() {
   const [data, setData] = useState({});
@@ -23,36 +24,61 @@ function App() {
     <span>En cours de chargement... </span>
   ) : (
     <>
-      <div className="wrapper">
-        <h2>{data.restaurant.name}</h2>
-        <img className="imgRestaurant" src={data.restaurant.picture} />
+      <nav>
+        <div className="wrapper">
+          <img className="imgLogo" src={logoDeliveroo} />
+        </div>
+      </nav>
 
-        <p>{data.restaurant.description}</p>
-        {data.categories
-          // Filter only categories with meals
-          .filter((category) => category.meals.length > 0)
-          .map((category) => {
-            return (
-              <section>
-                <h3>{category.name}</h3>
-                <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-                  {category.meals.map((meal) => {
-                    return (
-                      <div className="mealContainer">
-                        <h4>{meal.title}</h4>
-                        <p>{meal.description}</p>
-                        <p>{`${meal.price} €`}</p>
-                        {meal.picture && (
-                          <img className="imgMeal" src={meal.picture} />
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-              </section>
-            );
-          })}
-      </div>
+      <header>
+        <div className="wrapper">
+          <div className="mainContent">
+            <h1>{data.restaurant.name}</h1>
+            <p className="restaurantDescription">{data.restaurant.description}</p>
+          </div>
+          <div className="rightContent">
+              <img className="imgRestaurant" src={data.restaurant.picture} />
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="wrapper">
+          <div className="mainContent">
+            {data.categories
+              // Filter only categories with meals
+              .filter((category) => category.meals.length > 0)
+              .map((category) => {
+                return (
+                  <section>
+                    <h2>{category.name}</h2>
+                    <div
+                      style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}
+                    >
+                      {category.meals.map((meal) => {
+                        return (
+                          <div className="mealContainer">
+                            <div>
+                              <h3>{meal.title}</h3>
+                              <p className="mealDescription">
+                                {meal.description}
+                              </p>
+                              <p className="price">{`${meal.price} €`}</p>
+                            </div>
+                            {meal.picture && (
+                              <img className="imgMeal" src={meal.picture} />
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </section>
+                );
+              })}
+          </div>
+           <div className="rightContent">
+<h2>Panier</h2></div>
+        </div>
+      </main>
     </>
   );
 }
